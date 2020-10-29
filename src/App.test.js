9d6bf0a091409b2a0068d29d13d943d07c780356
yeuda.witman <yeudaww@gmail.com>
2020-10-29 21:55:48 +0200

    start tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { shallow } from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+//REDUX
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './redux/reducers';
+
+const store = createStore(rootReducer);
+
+describe('App component is working as expected', () => {
+
+  it('fake test', () => {
+      const wrapper = shallow(<Provider store={store}><App /></Provider>);
+      expect(wrapper.find(App).exists()).toBeTruthy();
+  });
 });
